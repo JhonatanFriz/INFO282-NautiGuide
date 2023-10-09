1fa742ff647972caf5263d6cdcdcc94d8198496f
YoanSalom <yoan.salom@gmail.com>
2023-10-09 16:55:29 -0300

    agregar barco completado

--- Frontend/src/components/barco/AgregarBarco.jsx
+++ Frontend/src/components/barco/AgregarBarco.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import clientAxios from '../config/clienteAxios';
+import { useNavigate } from "react-router-dom";
 
 function AgregarBarco() {
 
     
   const [nombre, setNombreBarco] = useState('');
-  const [imagen, setImagen] = useState('');
+  const [image, setImage] = useState('');
   const [modelo, setModelo] = useState('');
   const [personalizado, setPersonalizado] = useState('');
   const [error, setError] = useState(''); // Estado para el mensaje de error
 
+  const navigateToBack = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
 
     if (!personalizado) { // Verifica si el campo de rol está vacío
       setError('Seleccione si el barco es personalizado antes de enviar el registro.'); // Establece un mensaje de error
@@ -19,21 +22,20 @@ function AgregarBarco() {
     }
 
     try {
-      const { data } = await clientAxios.post(`/users`, {
+      const { data } = await clientAxios.post(`/barco`, {
         name: nombre,
-        image:imagen,
+        image: image,
         model: modelo,
-        password: contraseña,
         personalizado: personalizado,
       });
     } catch (error) {
       console.log(error);
     }
-    window.location.reload();
+    navigateToBack(`/barco`);
   };
 
   return (
-    <div className="p-4 bg-black shadow-md h-screen flex justify-center items-center ">
+    <div className="p-4 bg-blue-800 shadow-md h-screen flex justify-center items-center ">
       <div className="p-4 bg-white rounded-md border-8 w-1/2 j  ustify-center items-center">
       <h2 className="text-xl font-semibold mb-4 flex justify-center items-center ">Registrar Barco</h2>
       <form onSubmit={handleSubmit}>
@@ -54,11 +56,11 @@ function AgregarBarco() {
             Imagen
           </label>
           <input
-            type="image"
-            id="imagen"
+            type="iamge"
+            id="image"
             className="w-full px-4 py-2 rounded border focus:outline-none focus:border-blue-500"
-            value={imagen}
-            onChange={(e) => setImagen(e.target.value)}
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
           />
         </div>
         <div className="mb-4">
@@ -83,18 +85,21 @@ function AgregarBarco() {
             value={personalizado}
             onChange={(e) => setPersonalizado(e.target.value)}
           >
-            <option value="">Seleccione Si o No</option>
-            <option value="Si">SI</option>
+            <option value="">Seleccione Sí o No</option>
+            <option value="Si">SÍ</option>
             <option value="No">NO</option>
           </select>
         </div>
+
         {error && <p className="text-red-500">{error}</p>} {/* Muestra el mensaje de error si existe */}
+        <div className="flex justify-center items-center">
         <button
-          type="submit"
-          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-        >
-          Enviar
-        </button>
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 "
+          >
+            Añadir Barco
+            </button>
+        </div>
       </form>
     </div>
     </div>
