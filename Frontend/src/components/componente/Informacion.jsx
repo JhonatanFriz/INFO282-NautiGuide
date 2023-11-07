e1ac498c6ba4e03d9ec840d270122fdb67b54536
Jose Godoy <jose.godoy02@alumnos.uach.cl>
2023-11-07 16:09:03 -0300

    mensaje

--- Frontend/src/components/componente/Informacion.jsx
+++ Frontend/src/components/componente/Informacion.jsx
@@ -13,16 +13,20 @@ const informacion = ({componente, show}) => {
             setImagen(componente.image);
         }
     }, [show, componente]);
-    
+
     return(
-        <div className="bg-gray-200 flex flex-col justify-center items-center">
+        <div className="bg-gray-200 p-2 flex flex-col justify-center items-center space-y-2">
             
             {show && (
                 <>
                     <h2 className="text-xl font-semibold mb-2" style={{ textAlign: 'center', margin: '0' }}>
                         {nombre}
                     </h2>
-                    <img src={imagen} alt="Imagen" />
+                    <img
+                        src={imagen}
+                        alt="Imagen"
+                        style={{ width: '50%', height: 'auto' }}
+                    />
                     <h2 style={{ textAlign: 'center', margin: '0' }}>
                         {description}
                     </h2>
