1b04ca02fa0ac2963cb84d2fb78c9c9df1c09758
Manolo <jmgodoy14@gmail.com>
2023-11-18 22:47:33 -0300

    avance en 3d

--- Frontend/src/components/barco/Imagen3d.jsx
+++ Frontend/src/components/barco/Imagen3d.jsx
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import clientAxios from '../config/clienteAxios';
+
+import { Link } from "react-router-dom";
+
+
+import { Viewer } from '@photo-sphere-viewer/core';
 
 function Imagen3d(){
 
@@ -10,24 +15,43 @@ function Imagen3d(){
     const barcoId = barcoSeleccionado.id;
 
     const navigateTo = useNavigate();
-    const handleBarcosClick  = () => {
-        navigateTo(`/barco`);
-    }
 
     const handleBarcoClick  = () => {
         navigateTo(`/barco:${barcoId}/menuseccion`, {state: {barcoSeleccionado} });
     }
 
+       
+    useEffect(() => {
+        const baseUrl = 'https://photo-sphere-viewer-data.netlify.app/assets/';
+        
+        const viewer = new Viewer({
+            container: 'viewer',
+            panorama: baseUrl + 'sphere.jpg',
+            caption: 'Parc national du Mercantour <b>&copy; Damien Sorel</b>',
+            loadingImg: baseUrl + 'loader.gif',
+            touchmoveTwoFingers: true,
+            mousewheelCtrlKey: true,
+        });
+
+        const importMapConfig = {
+            "imports": {
+              "three": "https://cdn.jsdelivr.net/npm/three/build/three.module.js",
+              "@photo-sphere-viewer/core": "https://cdn.jsdelivr.net/npm/@photo-sphere-viewer/core/index.module.js"
+            }
+        };
+
+        return () => {
+            // Realiza cualquier limpieza necesaria al desmontar el componente
+            viewer.destroy();
+        };
+    }, []);
+
     return(
         <div className="bg-gray-100 flex flex-col h-screen">
             <h2 className="text-l px-2">
-                <a
-                    style={{cursor: 'pointer'}}
-                    onClick={handleBarcosClick}
-                    className="hover:underline focus:outline-none focus:ring focus:border-blue-300"
-                >
+                <Link to="/barco" className="hover:underline focus:outline-none focus:ring focus:border-blue-300">
                     Barcos
-                </a>
+                </Link>
                 /
                 <a
                     style={{cursor: 'pointer'}}
@@ -37,7 +61,11 @@ function Imagen3d(){
                     {barcoNombre}
                 </a>
                 /<strong className="font-bold">Imagen3d</strong>
-            </h2>        </div>
+            </h2>
+            <div className="flex justify-center py-10">
+                <div id="viewer" style={{ width: '80%', height: '50%' }}></div>
+            </div>
+        </div>
     )
 }
 export default Imagen3d
