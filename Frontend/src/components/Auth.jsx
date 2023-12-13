d66e73078e964dc2e431afe4f8a558504c4515ba
Jhonatan Friz <jhonatan.friz@alumnos.uach.cl>
2023-12-13 17:38:12 -0300

    a

--- Frontend/src/components/Auth.jsx
+++ Frontend/src/components/Auth.jsx
@@ -5,7 +5,6 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 import clientAxios from './config/clienteAxios';
 
-// ------------------------------------------------------
 const Auth = () => {
   const navigate = useNavigate();
   const { login, logout, token } = useAuth();
@@ -35,7 +34,7 @@ const Auth = () => {
         // window.location.reload();
         navigate('/barco');
         }, 1);
-        navigate('/barco');
+        // navigate('/barco');
         
       } else {
         setAuthError("Error: No se recibió un token en la respuesta del servidor");
@@ -71,80 +70,3 @@ const Auth = () => {
 };
 
 export default Auth;
-
-// --------------------------
-
-// import React, { useState } from 'react';
-// import { SyncLoader } from 'react-spinners';
-// import Formulario from './auth/Formulario';
-// import { useNavigate } from 'react-router-dom';
-// import { useAuth } from './AuthContext';
-// import clientAxios from './config/clienteAxios';
-
-// // ------------------------------------------------------
-// const Auth = () => {
-//   const navigate = useNavigate();
-//   const { login, logout, token } = useAuth();
-//   const [loading, setLoading] = useState(false);
-//   const [authError, setAuthError] = useState(null);
-
-//   const handleLogin = async (email, contraseña) => {
-//     try {
-//       setLoading(true);
-
-//       const response = await clientAxios.post(`/auth/login`, {
-//         mail: email,
-//         password: contraseña,
-//       });
-
-//       if (response.data && response.data.token) {
-//         const { token, role } = response.data;
-//         // login(token, role);
-//         localStorage.setItem('token', response.data.token);
-//         // login(response.data.user);
-//         login(token, role);
-//         await new Promise(resolve => setTimeout(resolve, 500));
-//         // console.log(response.data.user);
-//         // Simular un estado de carga de 1 segundo antes de recargar la página
-//         setTimeout(() => {
-//           // Recarga la página después de iniciar sesión
-//           // window.location.reload();
-//           navigate('/barco');
-//         }, 1);
-//         // await new Promise(resolve => setTimeout(resolve, 3000));
-//         navigate('/barco');
-        
-//       } else {
-//         setAuthError("Error: No se recibió un token en la respuesta del servidor");
-//       }
-//     } catch (error) {
-//       if (error.response && error.response.status === 401) {
-//         setAuthError("Error de autenticación: Verifica tu correo y contraseña");
-//       } else {
-//         setAuthError("Error de inicio de sesión: " + error.message);
-//       }
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-  
-//   const handleLogout = () => {
-//     logout();
-//     navigate('/auth');
-//   };
-
-//   return (
-//     <div className="bg-gradient-to-b from-sky-800 from-5% to-sky-200 to-95% h-screen flex justify-center items-center">
-//       <Formulario handleLogin={handleLogin} error={authError} />
-//       {loading && (
-//         <div className="flex justify-center items-center mt-4">
-//           <SyncLoader color={'#36D7B7'} loading={loading} size={10} />
-//           <p className="ml-2">Cargando...</p>
-//         </div>
-//       )}
-//       {/* {token && <button onClick={handleLogout}>Cerrar Sesión</button>} */}
-//     </div>
-//   );
-// };
-
-// export default Auth;
